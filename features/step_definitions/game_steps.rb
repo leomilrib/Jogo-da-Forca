7b564871e771a729c69d5d5d83d93f51ce442d58
Junior <nogueira_junior@yahoo.com.br>
2014-11-13 17:02:02 -0200

    Commit Inicial

---
+++ features/step_definitions/game_steps.rb
@@ -0,0 +1,13 @@
+# encoding: UTF-8
+
+Quando /^começo um novo jogo$/ do
+  steps %{ 
+    * I run 'forca' interactively 
+  }
+end
+
+Então /^vejo na tela:$/ do |text|
+  steps %{
+    * the stdout should contain "#{text}"
+  }
+end
