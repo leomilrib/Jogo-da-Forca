f352df4fbb96e1d0e72438fc71ef741cf3807cf8
Junior <nogueira_junior@yahoo.com.br>
2014-11-14 12:43:39 -0200

    Criação do game_helpers com um modulo que irá substituir o step do primeiro e implementará o terceiro cenário, reduzindo a duplicação através de support code no cucumber
    Alteração do game_step para chamar o game_helpers (start_new_game) ao invés de usar o step dentro dele

--- features/step_definitions/game_steps.rb
+++ features/step_definitions/game_steps.rb
@@ -1,9 +1,13 @@
 # encoding: UTF-8
 
 Quando /^começo um novo jogo$/ do
-  run_interactive("forca")
+  start_new_game
 end
 
 Então /^vejo na tela:$/ do |text|
   assert_partial_output(text, all_stdout)
+end
+
+Dado /^que comecei um jogo$/ do
+  start_new_game
 end
