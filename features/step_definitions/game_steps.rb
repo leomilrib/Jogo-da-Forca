fb6c6d9c4b83ba115a6e23752e6091651230c917
Junior <nogueira_junior@yahoo.com.br>
2014-11-14 13:45:32 -0200

    Mudança no game_step e começar jogo - erro
    Foi modificada a frase de dois cenários é para vir com duas falhas porém 1 está passando e o outro não

--- features/step_definitions/game_steps.rb
+++ features/step_definitions/game_steps.rb
@@ -4,15 +4,14 @@ Quando /^começo um novo jogo$/ do
   start_new_game
 end
 
-Então /^vejo na tela:$/ do |text|
-  assert_partial_output(text, all_stdout)
+Então /^o jogo termina com a seguinte mensagem na tela$/ do |text|
+  assert_passing_with(text)
 end
 
 Dado /^que comecei um jogo$/ do
   start_new_game
 end
 
-Quando /^escolho que a palavra a ser sorteada deverá ter "(.*?)" letras\
-$/ do |number_of_letters|
+Quando /^escolho que a palavra a ser sorteada deverá ter "(.*?)" letras$/ do |number_of_letters|
   type(number_of_letters)
 end
